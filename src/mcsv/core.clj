3085acc23dd8a3718f7aede8a1da2c2271fde479
Mindaugas Baranauskas <mindaugas.baranauskas@gmail.com>
2020-01-16 08:53:52 +0200

    parse delimiters

--- src/mcsv/core.clj
+++ src/mcsv/core.clj
@@ -2,6 +2,23 @@
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
+(def delimiters (hash-map ";" :semicolon
+                          "," :comma
+                          "\\t" :tab))
+
+(defn parse-delimiter 
+  [delimiter]
+  (get delimiters delimiter))
+
+(def cli-options
+  ;; An option with a required argument
+  [["-d" "--delimiter DELIMITER" "Delimiter symbol"
+    :default ";"
+    :parse-fn #(parse-delimiter %)
+    :validate [#(parse-delimiter %) "Must be valid delimiter"]]
+   ;; A non-idempotent option (:default is applied first)
+   ["-h" "--help"]])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
