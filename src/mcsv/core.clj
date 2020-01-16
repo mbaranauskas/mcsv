7107c2ee100c9ec24625a4a9907df33658568226
Mindaugas Baranauskas <mindaugas.baranauskas@gmail.com>
2020-01-16 09:25:46 +0200

    parse delimiters form command line.

--- src/mcsv/core.clj
+++ src/mcsv/core.clj
@@ -4,7 +4,7 @@
 
 (def delimiters (hash-map ";" :semicolon
                           "," :comma
-                          "\\t" :tab))
+                          "t" :tab))
 
 (defn parse-delimiter 
   [delimiter]
@@ -14,12 +14,13 @@
   ;; An option with a required argument
   [["-d" "--delimiter DELIMITER" "Delimiter symbol"
     :default ";"
-    :parse-fn #(parse-delimiter %)
-    :validate [#(parse-delimiter %) "Must be valid delimiter"]]
+    :parse-fn #(parse-delimiter %)]
+    ;:validate [#(parse-delimiter %) "Must be valid delimiter"]]
    ;; A non-idempotent option (:default is applied first)
    ["-h" "--help"]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [options (parse-opts args cli-options)]
+    (println (get-in options [:options :delimiter]))))
