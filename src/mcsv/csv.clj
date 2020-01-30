51986f614ad4619d99ca343f08b2142e78f59f58
Mindaugas Baranauskas <mindaugas.baranauskas@gmail.com>
2020-01-30 08:33:07 +0200

    Read csv into maps.

--- src/mcsv/csv.clj
+++ src/mcsv/csv.clj
@@ -3,9 +3,17 @@
    [clojure-csv.core :as csv]
    [clojure.java.io :as io]))
 
+(defn gen-fields []
+  (map #(str "Gen_field_" %) (drop 1 (range))))
+
+(defn header [csv-data]
+  (concat 
+   (first csv-data)
+   (gen-fields)))
+
 (defn csv-data->maps [csv-data]
   (map zipmap
-       (repeat (first csv-data))
+       (repeat (header csv-data))
        (rest csv-data)))
 
 (defn csv-file->maps [file-name delimiter]
--- test/mcsv/csv_test.clj
+++ test/mcsv/csv_test.clj
@@ -2,10 +2,41 @@
   (:require [clojure.test :refer :all]
             [mcsv.csv :refer :all]))
 
+(defn full-path [file-name]
+  (str "./dev-resources/" file-name))
+
 (deftest parse-csv-file
   (testing "Parse csv file to map"
     (is 
      (= 
-      (csv-file->maps "./dev-resources/sample-test.csv" \;) 
-      '({"Name" "Petras", "Surname" "Petraitis", "Birth year" "2000"} 
-       {"Name" "Jonas", "Surname" "Jonaitis", "Birth year" "2003"})))))
+      (csv-file->maps (full-path "sample-test.csv") \;) 
+      '({"Name" "Petras", 
+         "Surname" "Petraitis", 
+         "Birth year" "2000"} 
+        {"Name" "Jonas", 
+         "Surname" "Jonaitis", 
+         "Birth year" "2003"})))))
+
+(deftest csv-missing-fields
+  (testing "parse file with some records shorter than header"
+    (is
+     (=
+      (csv-file->maps (full-path "incomplete-records.csv") \;)
+      '({"Name" "Petras", 
+         "Surname" ""}
+        {"Name" "Jonas", 
+         "Surname" "Jonaitis", 
+         "Birth year" ""})))))
+
+(deftest csv-unexpected-fields
+  (testing "handle records with more fields than header"
+    (is
+     (= (csv-file->maps (full-path "unexpected-fields.csv") \;)
+        '({"Name" "Petras"
+           "Surname" "Petraitis"
+           "Birth year" "2000"}
+          {"Name" "Jonas"
+           "Surname" "Jonaitis"
+           "Birth year" "2003"
+           "Gen_field_1" "oops"
+           "Gen_field_2" "it is unexpected"})))))
